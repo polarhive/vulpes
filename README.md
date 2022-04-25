f5fb6a583ccd5829ed2d64f9340a376d10aa3108
Nathan Paul <polarhive@riseup.net>
2022-04-25 21:13:26 +0530

    add some more prefs

--- README.md
+++ README.md
@@ -16,17 +16,17 @@ vulpes - family of foxes; this user.js can be used with any Firefox based browse
 
 ## Like LibreWolf?
 
-In general, vanilla Firefox can be tweaked to do everything that [LibreWolf](https://librewolf.net/) does. Most [GNU/Linux](https://polarhive.ml/linux) distros ship with better, up-to-date versions of Firefox, browser addons receive critical auto-updates, some distros specifically add patches to improve performance / [integrate](https://gitlab.com/librewolf-community/browser/linux/-/issues/232) better with the [desktop like KDE](https://community.kde.org/Plasma/Browser_Integration). Most importantly you get official support from Mozilla, should you be facing bugs. LibreWolf might feel too harsh for some people, some sites stop working. In this case you should be using the [Tor Browser](https://www.torproject.org/) instead - for better privacy & compartmentalize your browsers.
+In general, vanilla Firefox can be tweaked to do everything that [LibreWolf](https://librewolf.net/) does. Most [GNU/Linux](https://polarhive.ml/linux) distros ship with better, up-to-date versions of Firefox, browser addons receive critical auto-updates, some distros specifically add patches to improve performance / [integrate](https://gitlab.com/librewolf-community/browser/linux/-/issues/232) better with the [desktop like KDE](https://community.kde.org/Plasma/Browser_Integration). Most importantly you get official support from Mozilla, should you be facing bugs. LibreWolf might feel too harsh for some people, some sites stop working. In this case you should be using the [Tor Browser](https://www.torproject.org/) instead. For better privacy, compartmentalize your browsers.
 
-So it's not really necessary to use LibreWolf if you can follow the simple instructions given here & you don't have a problem with software updates being hosted on Mozilla's servers.
+So it's not really necessary to use LibreWolf if you can follow the simple instructions given here & you don't have a problem with software updates being served from Mozilla's servers.
 
 ## Tweaks 🛠
 
 - Better Dark Theme
 - Telemetry disabled
 - Firefox accounts disabled
-- Google Safe browsing & Geolocation disabled
-- Reduced pinging 3rd party sites like pocket
+- Google Safe browsing & geolocation disabled
+- Stopped pinging 3rd party sites fetched by Pocket
 
 ---
 
@@ -38,7 +38,7 @@ So it's not really necessary to use LibreWolf if you can follow the simple instr
 2. Extract the ZIP
 3. Copy the ``user.js`` file to the clipboard
 4. Launch Firefox
-5. Type ``about:profiles`` in the addressbar & hit enter
+5. Type ``about:profiles`` in the address bar & hit enter
 6. Navigate to the Firefox current profile folder by clicking on ``Open Directory``
 7. Paste the ``user.js`` file into that Firefox profile directory
 8. Close Firefox & launch it again
@@ -47,7 +47,7 @@ So it's not really necessary to use LibreWolf if you can follow the simple instr
 
 Google, Bing, Yahoo are not good choices. Pick another search engine like [DuckDuckGo](https://duckduckgo.com/), [Whoogle](https://github.com/benbusby/whoogle-search), [Brave Search](https://search.brave.com/), [Wikipedia](https://wikipedia.org/), a [Searx](https://searx.me/) instance, or simply just bookmark sites.
 
-TIP -> Right click on the addressbar to add search engines
+TIP -> Right click on the address bar to add search engines
 
 ### 10. Extensions
 
@@ -63,7 +63,7 @@ TIP -> HTTPS is always on. Don't need to install any extensions.
 Check this page for updates. Bookmark it. [CTRL+D]
 
 ---
-It's a shame Mozilla bloats Firefox with every new update. Well at least it's better than anything on the market as of now. That being said, please tell Mozilla to not indulge in politics & divide users. I want them to actually fight for the 'Open Web' than act as a pawn for [Google](https://www.pcmag.com/news/mozilla-signs-lucrative-3-year-google-search-deal-for-firefox), slowly losing marketshare.
+It's a shame Mozilla bloats Firefox with every new update. Well at least it's better than anything on the market as of now. That being said, please tell Mozilla to not indulge in politics & divide users. I want them to actually fight for the 'Open Web' than act as a pawn for [Google](https://www.pcmag.com/news/mozilla-signs-lucrative-3-year-google-search-deal-for-firefox), slowly losing market share.
 
 ---
 This repo is hosted on [Codeberg](https://polarhive.ml/vulpes) & mirrored to [GitHub](https://polarhive.ml/github) for traffic.
--- user.js
+++ user.js
@@ -1,7 +1,6 @@
-
 // HOW TO USE?
+// Last updated on 2022-04-25
 // https://polarhive.ml/vulpes
-// Last updated on 2021-12-28
 
 user_pref("accessibility.typeaheadfind.flashBar", 0);
 user_pref("app.normandy.enabled", false);
@@ -26,6 +25,7 @@ user_pref("browser.download.autohideButton", true);
 user_pref("browser.download.panel.shown", true);
 user_pref("browser.eme.ui.enabled", false);
 user_pref("browser.engagement.ctrlTab.has-used", true);
+user_pref("browser.formfill.enable", false);
 user_pref("browser.laterrun.bookkeeping.profileCreationTime", 1640616291);
 user_pref("browser.laterrun.bookkeeping.sessionCount", 15);
 user_pref("browser.laterrun.enabled", true);
@@ -51,13 +51,13 @@ user_pref("browser.newtabpage.activity-stream.section.highlights.includeVisited"
 user_pref("browser.newtabpage.activity-stream.showSearch", false);
 user_pref("browser.newtabpage.activity-stream.showSponsored", false);
 user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false);
-user_pref("browser.newtabpage.activity-stream.telemetry.structuredIngestion.endpoint", "");
 user_pref("browser.newtabpage.activity-stream.telemetry", false);
+user_pref("browser.newtabpage.activity-stream.telemetry.structuredIngestion.endpoint", "");
 user_pref("browser.newtabpage.enabled", false);
 user_pref("browser.preferences.moreFromMozilla, false")
 user_pref("browser.safebrowsing.downloads.enabled", false);
-user_pref("browser.safebrowsing.downloads.remote.block_dangerous_host", false);
 user_pref("browser.safebrowsing.downloads.remote.block_dangerous", false);
+user_pref("browser.safebrowsing.downloads.remote.block_dangerous_host", false);
 user_pref("browser.safebrowsing.downloads.remote.block_potentially_unwanted", false);
 user_pref("browser.safebrowsing.downloads.remote.block_uncommon", false);
 user_pref("browser.safebrowsing.downloads.remote.enabled", false);
@@ -67,12 +67,14 @@ user_pref("browser.safebrowsing.provider.google.updateURL", "");
 user_pref("browser.safebrowsing.provider.google4.dataSharingURL", "");
 user_pref("browser.safebrowsing.provider.google4.gethashURL", "");
 user_pref("browser.safebrowsing.provider.google4.updateURL", "");
+user_pref("browser.shell.checkDefaultBrowser", false);
 user_pref("browser.shell.defaultBrowserCheckCount", 1);
 user_pref("browser.shell.didSkipDefaultBrowserCheckOnFirstRun", true);
 user_pref("browser.shell.mostRecentDateSetAsDefault", "1640620116");
-user_pref("browser.startup.homepage.abouthome_cache.enabled", true);
 user_pref("browser.startup.homepage", "about:blank");
+user_pref("browser.startup.homepage.abouthome_cache.enabled", true);
 user_pref("browser.tabs.crashReporting.sendReport", false);
+user_pref("browser.tabs.loadDivertedInBackground", true);
 user_pref("browser.tabs.warnOnClose", true);
 user_pref("browser.theme.content-theme", 0);
 user_pref("browser.theme.toolbar-theme", 0);
@@ -92,15 +94,16 @@ user_pref("devtools.theme.show-auto-theme-info", false);
 user_pref("dom.battery.enabled", false);
 user_pref("dom.push.connection.enabled", false);
 user_pref("dom.push.enabled", false);
-user_pref("dom.security.https_only_mode_ever_enabled", true);
-user_pref("dom.security.https_only_mode.upgrade_local", true);
 user_pref("dom.security.https_only_mode", true);
+user_pref("dom.security.https_only_mode.upgrade_local", true);
+user_pref("dom.security.https_only_mode_ever_enabled", true);
 user_pref("dom.webnotifications.enabled", false);
 user_pref("dom.webnotifications.serviceworker.enabled", false);
 user_pref("extensions.blacklist.enabled", false);
 user_pref("extensions.blocklist.enabled", false);
 user_pref("extensions.formautofill.addresses.enabled", false);
 user_pref("extensions.formautofill.available", "off");
+user_pref("extensions.formautofill.available", false);
 user_pref("extensions.formautofill.creditCards.available", false);
 user_pref("extensions.formautofill.creditCards.enabled", false);
 user_pref("extensions.getAddons.showPane", false);
@@ -130,28 +133,29 @@ user_pref("network.cookie.sameSite.laxByDefault", true);
 user_pref("network.cookie.sameSite.noneRequiresSecure", true);
 user_pref("network.cookie.sameSite.schemeful", true);
 user_pref("network.dns.disablePrefetch", true);
-user_pref("network.http.referer.defaultPolicy", 0);
-user_pref("network.http.referer.disallowCrossSiteRelaxingDefault", true);
 user_pref("network.http.referer.XOriginPolicy", 2);
 user_pref("network.http.referer.XOriginTrimmingPolicy", 2);
+user_pref("network.http.referer.defaultPolicy", 0);
+user_pref("network.http.referer.disallowCrossSiteRelaxingDefault", true);
 user_pref("network.http.speculative-parallel-limit", 0);
 user_pref("network.predictor.enabled", false);
 user_pref("network.prefetch-next", false);
-user_pref("pdfjs.enabledCache.state", false);
 user_pref("pdfjs.enableScripting", false);
+user_pref("pdfjs.enabledCache.state", false);
 user_pref("permissions.default.desktop-notification", 2);
 user_pref("permissions.default.geo", 2);
 user_pref("permissions.default.xr", 2);
 user_pref("pref.general.disable_button.default_browser", false);
 user_pref("privacy.annotate_channels.strict_list.enabled", true);
 user_pref("privacy.donottrackheader.enabled", true);
+user_pref("privacy.firstparty.isolate", true);
 user_pref("privacy.firstparty.isolate.block_post_message", true);
 user_pref("privacy.firstparty.isolate.use_site", true);
-user_pref("privacy.firstparty.isolate", true);
 user_pref("privacy.partition.network_state.ocsp_cache", true);
 user_pref("privacy.trackingprotection.enabled", true);
 user_pref("privacy.trackingprotection.socialtracking.enabled", true);
 user_pref("security.OCSP.enabled", 0);
+user_pref("signon.autofillForms", false);
 user_pref("toolkit.telemetry.reportingpolicy.firstRun", false);
 user_pref("toolkit.telemetry.server", "");
 user_pref("toolkit.telemetry.shutdownPingSender.enabled", false);
@@ -159,3 +163,4 @@ user_pref("trailhead.firstrun.didSeeAboutWelcome", true);
 user_pref("ui.systemUsesDarkTheme", 1);
 user_pref("widget.use-xdg-desktop-portal", true);
 user_pref("xpinstall.whitelist.required", false);
+
